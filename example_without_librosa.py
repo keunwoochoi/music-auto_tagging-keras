af8dcff798e445bbc23bcb179f45ea2ce573ce74
keunwoochoi <gnuchoi+github@gmail.com>
2016-08-06 17:37:00 -0400

    add example w.o. librosa

--- example_without_librosa.py
+++ example_without_librosa.py
@@ -7,8 +7,8 @@ def sort_result(tags, preds):
 	return sorted(result, key=lambda x:x[1], reverse=True)
 
 # setting
-audio_paths = ['data/bensound-cute.mp3', 'data/bensound-actionable.mp3', 'data/bensound-dubstep.mp3'
-, 'data/bensound-thejazzpiano.mp3']
+melgram_paths = ['data/bensound-cute.npy', 'data/bensound-actionable.npy', 'data/bensound-dubstep.npy'
+, 'data/bensound-thejazzpiano.npy']
 tags = ['rock', 'pop', 'alternative', 'indie', 'electronic', 'female vocalists', 
 		'dance', '00s', 'alternative rock', 'jazz', 'beautiful', 'metal', 
 		'chillout', 'male vocalists', 'classic rock', 'soul', 'indie rock',
@@ -20,8 +20,8 @@ tags = ['rock', 'pop', 'alternative', 'indie', 'electronic', 'female vocalists',
 
 # prepare data like this
 melgrams = np.zeros((0, 1, 96, 1366))
-for audio_path in audio_paths:
-	melgram = ap.compute_melgram(audio_path)
+for melgram_path in melgram_paths:
+	melgram = np.load(melgram_path)
 	melgrams = np.concatenate((melgrams, melgram), axis=0)
 
 # load model like this
@@ -31,9 +31,9 @@ model.load_weights('data/weights_best.hdf5')
 pred_tags = model.predict(melgrams)
 # print like this...
 print('Printing top-10 tags for each track...')
-for song_idx, audio_path in enumerate(audio_paths):
+for song_idx, melgram_path in enumerate(melgram_paths):
 	sorted_result = sort_result(tags, pred_tags[song_idx,:].tolist())
-	print(audio_path)
+	print(melgram_path)
 	print(sorted_result[:10])
 	print(' ')
 
