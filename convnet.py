3eaedbf3c4563a6d394a2b5b7760195428df2d94
keunwoochoi <gnuchoi+github@gmail.com>
2016-09-02 02:23:48 -0400

    add rnn

--- convnet.py
+++ convnet.py
@@ -25,13 +25,13 @@ def build_convnet_model():
 
 def design_2d_convnet_model():
 	num_channels = 1
-	height_input  =96
+	height_input = 96
 	width_input = 1366
 	num_conv_layers = 5
-	nums_feat_maps = [32,128,128,192,256]
+	nums_feat_maps = [32, 128, 128, 192, 256]
 	feat_scale_factor = 1
-	conv_sizes = [(3,3),(3,3),(3,3),(3,3),(3,3)]
-	pool_sizes = [(2,4),(2,4),(2,4),(3,5),(4,4)]
+	conv_sizes = [(3, 3), (3, 3), (3, 3), (3, 3), (3, 3)]
+	pool_sizes = [(2, 4), (2, 4), (2, 4), (3, 5), (4, 4)]
 	dropout_conv = 0.5
 	dim_labels = 50
 	# prepre modules
@@ -56,14 +56,12 @@ def get_convBNeluMPdrop(num_conv_layers, nums_feat_maps, feat_scale_factor, conv
 		# add conv layer
 		n_feat_here = int(nums_feat_maps[conv_idx]*feat_scale_factor)
 		if not input_shape_specified:
-			print ' ---->>First conv layer is being added! with %d' % n_feat_here
 			model.add(Convolution2D(n_feat_here, conv_sizes[conv_idx][0], conv_sizes[conv_idx][1], 
 									input_shape=input_shape,
 									border_mode='same',  
 									init='he_normal'))
 			input_shape_specified = True
 		else:
-			print ' ---->>%d-th conv layer is being added with %d units' % (conv_idx, n_feat_here)
 			model.add(Convolution2D(n_feat_here, conv_sizes[conv_idx][0], conv_sizes[conv_idx][1], 
 									border_mode='same',
 									init='he_normal'))
@@ -74,7 +72,4 @@ def get_convBNeluMPdrop(num_conv_layers, nums_feat_maps, feat_scale_factor, conv
 		model.add(MaxPooling2D(pool_size=pool_sizes[conv_idx]))
 		if not dropout_conv == 0.0:
 			model.add(Dropout(dropout_conv))
-			print ' ---->>Add dropout of %f for %d-th conv layer' % (dropout_conv, conv_idx)
-	model.summary()
 	return model
-
--- example.py
+++ example.py
@@ -1,40 +1,62 @@
 import numpy as np
-import convnet
+import convnet, recurrentnet
 import audio_processor as ap
+import pdb
 
 def sort_result(tags, preds):
 	result = zip(tags, preds)
-	return sorted(result, key=lambda x:x[1], reverse=True)
+	sorted_result = sorted(result, key=lambda x:x[1], reverse=True)
+	return [(name, '%5.3f'%score) for name, score in sorted_result]
 
-# setting
-audio_paths = ['data/bensound-cute.mp3', 'data/bensound-actionable.mp3', 'data/bensound-dubstep.mp3'
-, 'data/bensound-thejazzpiano.mp3']
-tags = ['rock', 'pop', 'alternative', 'indie', 'electronic', 'female vocalists', 
-		'dance', '00s', 'alternative rock', 'jazz', 'beautiful', 'metal', 
-		'chillout', 'male vocalists', 'classic rock', 'soul', 'indie rock',
-		'Mellow', 'electronica', '80s', 'folk', '90s', 'chill', 'instrumental',
-		'punk', 'oldies', 'blues', 'hard rock', 'ambient', 'acoustic', 'experimental',
-		'female vocalist', 'guitar', 'Hip-Hop', '70s', 'party', 'country', 'easy listening',
-		'sexy', 'catchy', 'funk', 'electro' ,'heavy metal', 'Progressive rock',
-		'60s', 'rnb', 'indie pop', 'sad', 'House', 'happy']
+def main(net):
+	print('Running main() with network: %s' % net)
+	# setting
+	audio_paths = ['data/bensound-cute.mp3', 'data/bensound-actionable.mp3', 'data/bensound-dubstep.mp3'
+	, 'data/bensound-thejazzpiano.mp3']
+	tags = ['rock', 'pop', 'alternative', 'indie', 'electronic', 'female vocalists', 
+			'dance', '00s', 'alternative rock', 'jazz', 'beautiful', 'metal', 
+			'chillout', 'male vocalists', 'classic rock', 'soul', 'indie rock',
+			'Mellow', 'electronica', '80s', 'folk', '90s', 'chill', 'instrumental',
+			'punk', 'oldies', 'blues', 'hard rock', 'ambient', 'acoustic', 'experimental',
+			'female vocalist', 'guitar', 'Hip-Hop', '70s', 'party', 'country', 'easy listening',
+			'sexy', 'catchy', 'funk', 'electro' ,'heavy metal', 'Progressive rock',
+			'60s', 'rnb', 'indie pop', 'sad', 'House', 'happy']
 
-# prepare data like this
-melgrams = np.zeros((0, 1, 96, 1366))
-for audio_path in audio_paths:
-	melgram = ap.compute_melgram(audio_path)
-	melgrams = np.concatenate((melgrams, melgram), axis=0)
+	# prepare data like this
+	melgrams = np.zeros((0, 1, 96, 1366))
+	for audio_path in audio_paths:
+		melgram = ap.compute_melgram(audio_path)
+		melgrams = np.concatenate((melgrams, melgram), axis=0)
 
-# load model like this
-model = convnet.build_convnet_model()
-model.load_weights('data/weights_best.hdf5')
-# predict the tags like this
-pred_tags = model.predict(melgrams)
-# print like this...
-print('Printing top-10 tags for each track...')
-for song_idx, audio_path in enumerate(audio_paths):
-	sorted_result = sort_result(tags, pred_tags[song_idx,:].tolist())
-	print(audio_path)
-	print(sorted_result[:10])
-	print(' ')
+	# load model like this
+	if net == 'cnn':
+		model = convnet.build_convnet_model()
+	elif net == 'rnn':
+		model = recurrentnet.build_recurrentnet_model()
+
+	model.summary()
+	print('Loading weights of %s...' % net)
+	model.load_weights('data/%s_weights_best.hdf5' % net)
+	pdb.set_trace()
+	# predict the tags like this
+	print('Predicting...')
+	pred_tags = model.predict(melgrams)
+	# print like this...
+	print('Printing top-15 tags for each track...')
+	for song_idx, audio_path in enumerate(audio_paths):
+		sorted_result = sort_result(tags, pred_tags[song_idx,:].tolist())
+		print(audio_path)
+		print(sorted_result[:5])
+		print(sorted_result[5:10])
+		print(sorted_result[10:15])
+		print(' ')
+	return
+
+if __name__=='__main__':
+
+	networks = ['rnn', 'cnn']
+	for net in networks:
+		main(net)
+
 
 
